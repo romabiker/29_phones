f38912f5407397dd009ff11158d57e66fa2f7371
Valery Ramusik <valer2677.@gmailcom>
2017-11-28 22:56:40 +0300

    Simplifies watch func

--- phone_normalizer.py
+++ phone_normalizer.py
@@ -25,33 +25,34 @@ def normalize_to_national_number(phonenumber, country='RU'):
     return phonenumber_obj.national_number
 
 
-def watch_prod_db(delay=2*60, first=0, counter=5, default_tries=5, step=1):
+def query_not_normalized_orders(delay=60, tries=5, step=1):
     while True:
         try:
-            orders_to_normalize_phone = session.query(Order).filter(
+            return session.query(Order).filter(
                 Order.normalized_phone.is_(None)).all()
-            counter = default_tries
-            orders_amount_to_norm = len(orders_to_normalize_phone)
-            if not orders_amount_to_norm:
-                logging.info('sleeping {delay}'.format(delay=delay))
-                time.sleep(delay)
-            else:
-                logging.info('{orders_amount_to_norm} orders to normalize'.format(
-                    orders_amount_to_norm=orders_amount_to_norm))
-                for order in orders_to_normalize_phone:
-                    order.normalized_phone = normalize_to_national_number(
-                                                  order.contact_phone)
-                    session.add(order)
-                session.commit()
-                logging.info('After commit sleeping {delay}'.format(delay=delay))
-                time.sleep(delay)
         except exc.DBAPIError as error:
-            counter -= step
-            if not counter:
-                logging.error(
-                    'There is {error_msg} after {default_tries} tries'.format(
-                        error_msg=error.msg, default_tries=default_tries))
-                break
+            tries -= step
+            if not tries:
+                raise exc.DBAPIError
+            time.sleep(delay)
+
+
+def watch_prod_db(delay=2*60):
+    while True:
+        orders_to_normalize_phone = query_not_normalized_orders()
+        orders_amount_to_norm = len(orders_to_normalize_phone)
+        if not orders_amount_to_norm:
+            logging.info('sleeping {delay}'.format(delay=delay))
+            time.sleep(delay)
+        else:
+            logging.info('{orders_amount_to_norm} orders to normalize'.format(
+                orders_amount_to_norm=orders_amount_to_norm))
+            for order in orders_to_normalize_phone:
+                order.normalized_phone = normalize_to_national_number(
+                                              order.contact_phone)
+                session.add(order)
+            session.commit()
+            logging.info('After commit sleeping {delay}'.format(delay=delay))
             time.sleep(delay)
 
 
